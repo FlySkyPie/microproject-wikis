382c0cd6f530ee0fb28781f889785cb35e5fa81c
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2026-03-05 18:18:12 +0800

    docs(utilities): add todo items

---
+++ utilities/scripts/combine-builds.ts
@@ -0,0 +1,8 @@
+import fs from 'node:fs/promises';
+
+export const readWikis = async (): Promise<string[]> => {
+  /**
+   * @todo Return path of folders under `wikis/` exclude `template`.
+   */
+  return [];
+}
